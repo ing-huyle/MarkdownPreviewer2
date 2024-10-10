9df7ae03eb28c12c60019c3f22140b600b8bbe14
Huy Le <ing.huyle@gmail.com>
2024-10-10 14:24:33 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import './styles/App.scss'
-import Editor from './Editor';
-import Previewer from './Previewer';
+import './styles/App.scss';
+import $ from "jQuery";
+import initialValue from './initialValue';
+import { marked } from 'marked';
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [value, setValue] = useState(initialValue);
+  
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  }
+
+  useEffect(() => {
+    $("#preview").html(marked.parse(value));
+  }, [value]);
 
   return (
     <div className="app-wrapper">
-      <Editor />
-      <Previewer />
+      <div className="editor-wrapper">
+        <div className="titlebar">
+          <div>Editor</div>
+          <div className="maximize"></div>
+        </div>
+        <textarea id="editor" value={value} onChange={handleChange}></textarea>
+      </div>
+      <div className="preview-wrapper">
+        <div className="titlebar">
+          <div>Preview</div>
+          <div className="maximize"></div>
+        </div>
+        <div id="preview"></div>
+    </div>
     </div>
   )
 }
